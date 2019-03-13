fea53ecf5e68758d02704c431a9ae76c5c596a97
Eunice Min <euniceem1007@gmail.com>
2019-03-12 14:38:37 -1000

    changed app.css to scss

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = (props) => {
+
+  return (
+    <div className="header-container">
+
+      <div className="menu-icon">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Header;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 import Navigation from '../../components/Navigation';
 import Feed from '../../containers/Feed';
