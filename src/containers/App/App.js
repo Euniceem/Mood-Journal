d30699b1dc21e5a0d983ea2f5b9a4228fcf0d03c
Eunice Min <euniceem1007@gmail.com>
2019-03-12 14:17:54 -1000

    added in navigation component and routes for data, feed, and calender

---
+++ src/containers/App/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+
+import Navigation from '../../components/Navigation';
+import Feed from '../../containers/Feed';
+import Data from '../../containers/Data';
+import Calender from '../../containers/Calender';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <>
+            <Switch>
+              <Route exact={true} path='/feed' component={Feed} />
+              <Route exact={true} path='/data' component={Data} />
+              <Route exact={true} path='/calender' component={Calender} />
+            </Switch>
+            <Navigation />
+          </>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
