2562547d3cd0e3ed836e26b1f647921771ce0481
Gene83 <gene83@protonmail.com>
2019-03-13 19:57:58 -1000

    update tables and seeds to include fewer default actions and activties which are always displayed

--- database/seeds/default_activities.js
+++ database/seeds/default_activities.js
@@ -5,16 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('default_activities').insert([
-        { name: 'work', is_displayed: true },
-        { name: 'school', is_displayed: true },
-        { name: 'relaxation', is_displayed: true },
-        { name: 'friends', is_displayed: true },
-        { name: 'sports', is_displayed: true },
-        { name: 'party', is_displayed: true },
-        { name: 'movies', is_displayed: true },
-        { name: 'reading', is_displayed: true },
-        { name: 'shopping', is_displayed: true },
-        { name: 'travel', is_displayed: true }
+        { name: 'work' },
+        { name: 'school' },
+        { name: 'relaxation' },
+        { name: 'friends' },
+        { name: 'sports' }
       ]);
     });
 };
--- database/seeds/default_emotions.js
+++ database/seeds/default_emotions.js
@@ -5,18 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('default_emotions').insert([
-        { name: 'energy', is_displayed: true },
-        { name: 'happiness', is_displayed: true },
-        { name: 'focus', is_displayed: true },
-        { name: 'excitment', is_displayed: false },
-        { name: 'enjoyment', is_displayed: false },
-        { name: 'relaxation', is_displayed: false },
-        { name: 'calmness', is_displayed: false },
-        { name: 'fatigue', is_displayed: false },
-        { name: 'boredom', is_displayed: false },
-        { name: 'sadness', is_displayed: false },
-        { name: 'stress', is_displayed: false },
-        { name: 'anxiety', is_displayed: false }
+        { name: 'energy' },
+        { name: 'happiness' },
+        { name: 'anxiety' },
+        { name: 'stress' }
       ]);
     });
 };
