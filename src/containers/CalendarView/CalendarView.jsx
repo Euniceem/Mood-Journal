48502bd8a9199e74f616f42a54cfe63268254547
Eunice Min <euniceem1007@gmail.com>
2019-03-13 15:30:56 -1000

    improved scss and got dummy entry to show up when clicked on a certain day

--- src/containers/CalendarView/CalendarView.jsx
+++ src/containers/CalendarView/CalendarView.jsx
@@ -3,20 +3,30 @@ import './CalendarView.scss';
 import Header from '../../components/Header';
 import Calendar from 'react-calendar';
 
-
 class CalendarView extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
       date: new Date(),
+      showDayEntry: false
     }
 
-    this.onChange = this.onChange.bind(this)
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onChangeDay = this.onChangeDay.bind(this);
   }
 
-  onChange(date) {
+  onChangeDate(date) {
     this.setState({ date })
+  }
+
+  //only shows entries before or on current date/time
+  onChangeDay(e) {
+    if (e >= new Date()) {
+      this.setState({ showDayEntry: false })
+    } else {
+      this.setState({ showDayEntry: true })
+    }
   }
 
   render() {
@@ -25,13 +35,22 @@ class CalendarView extends Component {
         <Header />
         <h1 className="title">Calendar</h1>
         <Calendar
-          onChange={this.onChange}
+          onChange={this.onChangeDate}
           value={this.state.date}
           calendarType="US"
           className="calendar-view"
           minDetail="decade"
           view="month"
+          onClickDay={this.onChangeDay}
         />
+        {this.state.showDayEntry === true ?
+          <div className="calendar-day-entry-container">
+            <div className="day-entry-title">DATE/TIME</div>
+
+          </div>
+          :
+          null
+        }
       </div>
     )
   }
