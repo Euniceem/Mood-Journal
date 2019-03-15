bc299f4196e4fdd69b2638f298e871bd0154aa55
Eunice Min <euniceem1007@gmail.com>
2019-03-14 19:52:57 -1000

    changed drop menu to show logout or login depending if the user is logged in or not

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import { Link, withRouter } from 'react-router-dom';
+import { logout } from '../../actions';
 import './Header.scss';
 
 class Header extends Component {
@@ -10,7 +12,9 @@ class Header extends Component {
       showMenu: false
     }
 
+
     this.handleMenu = this.handleMenu.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   handleMenu() {
@@ -19,8 +23,17 @@ class Header extends Component {
     })
   }
 
-  render() {
+  handleLogout(e) {
+    e.preventDefault();
 
+    this.props.logout()
+      .then(() => {
+
+        this.props.history.push('/login');
+      })
+  }
+
+  render() {
     return (
       <div className="header-container">
         <div className="nav-menu-container">
@@ -37,8 +50,13 @@ class Header extends Component {
                 >
                   <Link to="/feed" className="link">Home</Link>
                   <Link to="/settings" className="link">Settings</Link>
-                  <Link to="/login" className="link">Login</Link>
-                  <div className="link">Logout</div>
+
+                  {!this.props.email && this.props.isLoggedIn === false
+                    ?
+                    <Link to="/login" className="link">Login</Link>
+                    :
+                    <div className="link" onClick={this.handleLogout}>Logout</div>
+                  }
                 </div>
               )
               : (
@@ -55,4 +73,27 @@ class Header extends Component {
   }
 }
 
-export default Header;
+
+const mapStateToProps = (state) => {
+
+  return {
+    email: state.email,
+    isLoggedIn: state.loggedIn,
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: user => {
+      return dispatch(logout(user))
+    }
+  }
+}
+
+Header = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
+
+export default withRouter(Header);
