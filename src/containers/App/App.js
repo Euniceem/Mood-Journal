ffe13df33529e0d1b74545cb207bbee1aec63221
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-03-16 18:57:08 -1000

    safe commit for merge

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
 
+import { generateEntries } from '../../actions';
+
 import MoodEntry from '../MoodEntry';
 import Navigation from '../../components/Navigation';
 import Feed from '../../containers/Feed';
@@ -33,11 +35,64 @@ const PrivateRoute = ({ component: Component, isAuth, ...rest }) => {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.generateEntries = this.generateEntries.bind(this);
+  }
+
+  generateEntries() {
+    const start = new Date('2019-01-01 20:19:21.637+00');
+    const end = new Date();
+  
+    for (let i = 0; i < 216; i++) {
+      let entry = {
+        mood_id: Math.ceil(Math.random() * 5),
+        notes: `Lorem ipsum dolor sit amet`,
+        default_activities: [
+          Math.ceil(Math.random() * 3),
+          Math.ceil(Math.random() * 2 + 3)
+        ],
+        custom_activities: [Math.ceil(Math.random() * 3)],
+        default_emotions: [
+          {
+            default_emotion_id: 1,
+            percent: Math.ceil(Math.random() * 100)
+          },
+          {
+            default_emotion_id: 2,
+            percent: Math.ceil(Math.random() * 100)
+          },
+          {
+            default_emotion_id: 3,
+            percent: Math.ceil(Math.random() * 100)
+          },
+          {
+            default_emotion_id: 4,
+            percent: Math.ceil(Math.random() * 100)
+          }
+        ],
+        custom_emotions: [
+          {
+            custom_emotion_id: Math.ceil(Math.random() * 2),
+            percent: Math.ceil(Math.random() * 100)
+          }
+        ],
+        created_at: new Date(
+          start.getTime() + Math.random() * (end.getTime() - start.getTime())
+        )
+      };
+  
+      this.props.generateEntries(entry);
+    }
+  }
+
   render() {
     let isAuthenticated = this.props.email ? this.props.isLoggedIn : null
 
     return (
       <div className="App">
+        <button onClick={ this.generateEntries }>asdjadsjklasdjkl</button>
         <Router>
           <>
             <Switch>
@@ -64,8 +119,17 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    generateEntries : entry => {
+      dispatch(generateEntries(entry));
+    }
+  }
+}
+
 App = connect(
   mapStateToProps,
+  mapDispatchToProps
 )(App);
 
 export default App;
