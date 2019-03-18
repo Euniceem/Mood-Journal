f26fddc161ead3cb8dc2f0f829b63c943a51a2fc
Gene83 <gene83@protonmail.com>
2019-03-18 10:30:00 -1000

    add toggle button for switching views between mood and emotions

--- src/containers/Data/Data.jsx
+++ src/containers/Data/Data.jsx
@@ -17,7 +17,20 @@ class Data extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      chart_type: 'mood',
+      display_type: 'average',
+      time: 'today'
+    };
+
+    this.handleOptionsOnClick = this.handleOptionsOnClick.bind(this);
+  }
+
+  handleOptionsOnClick(e) {
+    const inputType = e.target.dataset.inputType;
+    const value = e.target.value;
+
+    this.setState({ [inputType]: value });
   }
 
   componentDidMount() {
@@ -30,7 +43,13 @@ class Data extends Component {
 
     for (let day in this.props.avgWeek) {
       this.props.avgWeek[day].day = parseInt(day);
-      moodData.push(this.props.avgWeek[day]);
+      moodData.push({
+        mood: this.props.avgWeek[day].moodSum,
+        anxiety: this.props.avgWeek[day].emotions.anxiety,
+        energy: this.props.avgWeek[day].emotions.energy,
+        happiness: this.props.avgWeek[day].emotions.happiness,
+        index: this.props.avgWeek[day]
+      });
     }
 
     for (let day in this.props.avgWeek) {
@@ -41,8 +60,30 @@ class Data extends Component {
     return (
       <div className="data-container">
         <Header />
+        <div className="mood-or-emotions-switch">
+          <button
+            className="btn"
+            data-input-type="chart_type"
+            value="mood"
+            onClick={this.handleOptionsOnClick}
+          >
+            Mood
+          </button>
+          <button
+            className="btn"
+            data-input-type="chart_type"
+            value="emotions"
+            onClick={this.handleOptionsOnClick}
+          >
+            Emotions
+          </button>
+        </div>
+
         <h1 className="title">Data</h1>
         <LineChart
+          className={
+            this.state.chart_type === 'mood' ? 'chart show' : 'chart hide'
+          }
           width={350}
           height={200}
           data={moodData}
@@ -51,11 +92,15 @@ class Data extends Component {
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="day" />
           <YAxis />
-          <Line type="monotone" dataKey="moodSum" stroke="#82ca9d" />
+          <Legend />
+          <Line type="monotone" dataKey="mood" stroke="#82ca9d" />
           <Tooltip />
         </LineChart>
 
         <LineChart
+          className={
+            this.state.chart_type === 'emotions' ? 'chart show' : 'chart hide'
+          }
           width={350}
           height={200}
           data={emotionData}
