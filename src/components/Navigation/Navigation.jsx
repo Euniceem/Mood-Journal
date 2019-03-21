84d11e66606eedfa58940e21f53f7a3ed92fff73
Eunice Min <euniceem1007@gmail.com>
2019-03-20 18:45:43 -1000

    added tagglehomeview functionality so it switches based on what the user chooses

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,25 +3,79 @@ import { Link } from 'react-router-dom';
 import './Navigation.scss';
 
 const Navigation = (props) => {
-
-
   return (
+
     <footer className="navigation-container">
 
-      <div className="data">
-        <Link to="/data" className="link"> DATA</Link>
-      </div>
+      {props.setHomePage === "calendar" ?
+        <div className="navbar">
+          <div className="data">
+            <Link to="/data" className="link"> DATA</Link>
+          </div>
 
-      <div className="feed">
-        <Link to="/feed" className="link"> FEED </Link>
-      </div>
+          <div id="home" className="calendar">
+            <Link to="/calendar" className="link"> CALENDAR </Link>
+          </div>
 
-      <div className="calendar">
-        <Link to="/calendar" className="link"> CALENDAR </Link>
-      </div>
+          <div className="feed">
+            <Link to="/feed" className="link"> FEED </Link>
+          </div>
+        </div>
+        : null
+      }
+
+      {props.setHomePage === "feed" ?
+        <div className="navbar">
+          <div className="data">
+            <Link to="/data" className="link"> DATA</Link>
+          </div>
+
+          <div id="home" className="feed">
+            <Link to="/feed" className="link"> FEED </Link>
+          </div>
+
+          <div className="calendar">
+            <Link to="/calendar" className="link"> CALENDAR </Link>
+          </div>
+        </div>
+        : null
+      }
+
+      {props.setHomePage === "data" ?
+        <div className="navbar">
+          <div className="feed">
+            <Link to="/feed" className="link"> FEED </Link>
+          </div>
+
+          <div id="home" className="data">
+            <Link to="/data" className="link"> DATA</Link>
+          </div>
+
+          <div className="calendar">
+            <Link to="/calendar" className="link"> CALENDAR </Link>
+          </div>
+        </div>
+        : null
+      }
+
+      {props.setHomePage === null ?
+        <div className="navbar">
+          <div className="data">
+            <Link to="/data" className="link"> DATA</Link>
+          </div>
+
+          <div id="home" className="feed">
+            <Link to="/feed" className="link"> FEED </Link>
+          </div>
+
+          <div className="calendar">
+            <Link to="/calendar" className="link"> CALENDAR </Link>
+          </div>
+        </div>
+        : null
+      }
 
     </footer>
-
   )
 }
 
