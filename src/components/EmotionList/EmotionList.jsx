033fa266bb14d0c8b670adf6dd913434c25a3abb
Eunice Min <euniceem1007@gmail.com>
2019-03-22 21:41:41 -1000

    finished styling

--- src/components/EmotionList/EmotionList.jsx
+++ src/components/EmotionList/EmotionList.jsx
@@ -3,21 +3,25 @@ import React from 'react';
 const EmotionList = (props) => {
   console.log(props.emotions);
 
-  const emotionList = props.emotions.map((emotions, index) => 
-  {
+  const emotionList = props.emotions.map((emotions, index) => {
     console.log(emotions.default_emotion);
-      return (
-        <div className="single-entry-view-emotion" key={index}>
+    return (
+      <div className="single-entry-view-emotion" key={index}>
+        <div className="emotion-name">
           {emotions.custom_emotion
             ? emotions.custom_emotion.name
-          : null }
+            : null}
 
           {emotions.default_emotion
             ? emotions.default_emotion.name
             : null
-          }
+          }:
         </div>
-      );
+        <div className="emotion-percent">
+          {emotions.percent} %
+        </div>
+      </div>
+    );
   })
 
   return (
