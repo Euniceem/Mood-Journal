31ee9261f700fab3e9407eb6f9bcedd60d3b866b
Gene83 <gene83@protonmail.com>
2019-03-22 15:09:26 -1000

    delete console.log

--- src/components/SingleEntryFeed/SingleEntryFeed.jsx
+++ src/components/SingleEntryFeed/SingleEntryFeed.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import './SingleEntryFeed.scss';
 
 const SingleEntryFeed = props => {
-  console.log(props);
   let date = new Date(props.entryData.created_at);
   let time = new Date(props.entryData.created_at);
 
